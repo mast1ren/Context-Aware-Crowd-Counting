4175877ae3c6e043ff3f2398cabc12f86f15dc47
mast1ren <masterenlu@outlook.com>
2022-10-12 02:22:50 +0000

    format test file

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ from torch.autograd import Variable
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 
 from torchvision import transforms
+import json
 
 
 transform=transforms.Compose([
@@ -20,17 +21,20 @@ transform=transforms.Compose([
                    ])
 
 # the folder contains all the test images
-img_folder='../data/part_B_final/test_data/images'
-img_paths=[]
+# img_folder='../data/part_B_final/test_data/images'
+# img_paths=[]
 
-for img_path in glob.glob(os.path.join(img_folder, '*.jpg')):
-    img_paths.append(img_path)
+# for img_path in glob.glob(os.path.join(img_folder, '*.jpg')):
+#     img_paths.append(img_path)
+
+with open('test.json','r') as f:
+    img_paths=json.load(f)
 
 model = CANNet()
 
 model = model.cuda()
 
-checkpoint = torch.load('part_B_pre.pth.tar')
+checkpoint = torch.load('0model_best.pth.tar')
 
 model.load_state_dict(checkpoint['state_dict'])
 
@@ -38,31 +42,31 @@ model.eval()
 
 pred= []
 gt = []
-
-for i in xrange(len(img_paths)):
-    img = transform(Image.open(img_paths[i]).convert('RGB')).cuda()
-    img = img.unsqueeze(0)
-    h,w = img.shape[2:4]
-    h_d = h/2
-    w_d = w/2
-    img_1 = Variable(img[:,:,:h_d,:w_d].cuda())
-    img_2 = Variable(img[:,:,:h_d,w_d:].cuda())
-    img_3 = Variable(img[:,:,h_d:,:w_d].cuda())
-    img_4 = Variable(img[:,:,h_d:,w_d:].cuda())
-    density_1 = model(img_1).data.cpu().numpy()
-    density_2 = model(img_2).data.cpu().numpy()
-    density_3 = model(img_3).data.cpu().numpy()
-    density_4 = model(img_4).data.cpu().numpy()
-
-    pure_name = os.path.splitext(os.path.basename(img_paths[i]))[0]
-    gt_file = h5py.File(img_paths[i].replace('.jpg','.h5').replace('images','ground_truth'),'r')
-    groundtruth = np.asarray(gt_file['density'])
-    pred_sum = density_1.sum()+density_2.sum()+density_3.sum()+density_4.sum()
-    pred.append(pred_sum)
-    gt.append(np.sum(groundtruth))
-
+with torch.no_grad():
+    for i in range(len(img_paths)):
+        img = transform(Image.open(img_paths[i]).convert('RGB')).cuda()
+        img = img.unsqueeze(0)
+        h,w = img.shape[2:4]
+        h_d = h/2
+        w_d = w/2
+        img_1 = Variable(img[:,:,:h_d,:w_d].cuda())
+        img_2 = Variable(img[:,:,:h_d,w_d:].cuda())
+        img_3 = Variable(img[:,:,h_d:,:w_d].cuda())
+        img_4 = Variable(img[:,:,h_d:,w_d:].cuda())
+        density_1 = model(img_1).data.cpu().numpy()
+        density_2 = model(img_2).data.cpu().numpy()
+        density_3 = model(img_3).data.cpu().numpy()
+        density_4 = model(img_4).data.cpu().numpy()
+    
+        # pure_name = os.path.splitext(os.path.basename(img_paths[i]))[0]
+        gt_file = h5py.File(img_paths[i].replace('.jpg','.h5').replace('data','annotation'),'r')
+        groundtruth = np.asarray(gt_file['density'])
+        pred_sum = density_1.sum()+density_2.sum()+density_3.sum()+density_4.sum()
+        pred.append(pred_sum)
+        gt.append(np.sum(groundtruth))
+    
 mae = mean_absolute_error(pred,gt)
 rmse = np.sqrt(mean_squared_error(pred,gt))
 
-print 'MAE: ',mae
-print 'RMSE: ',rmse
+print ('MAE: ',mae)
+print ('RMSE: ',rmse)

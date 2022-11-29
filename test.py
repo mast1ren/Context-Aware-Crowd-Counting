d06326eac617b9e2f46777de5010c0eaa9884b40
mast1ren <masterenlu@outlook.com>
2022-11-29 07:02:15 +0000

    save result

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ def get_seq_class(seq, set):
 with open('test.json','r') as f:
     img_paths=json.load(f)
 
-device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 print(device)
 
 model = CANNet()
@@ -75,14 +75,15 @@ model.eval()
 
 pred= []
 gt = []
-preds = [[] for i in range(10)]
-gts = [[] for i in range(10)]
+preds = [[] for i in range(8)]
+gts = [[] for i in range(8)]
 max_error = 0
 min_error = 1000
 with torch.no_grad():
     for i in range(len(img_paths)):
         img_path = os.path.join('../../ds/dronebird', img_paths[i])
         seq = int(os.path.basename(img_path)[3:6])
+        seq = 'DJI_' + str(seq).zfill(4)
         light, angle, bird, size = get_seq_class(seq, 'test')
         gt_path = os.path.join(os.path.dirname(img_path).replace('images', 'ground_truth'), 'GT_'+os.path.basename(img_path).replace('jpg', 'h5'))
 
@@ -106,8 +107,9 @@ with torch.no_grad():
         pred_sum = density_1.sum()+density_2.sum()+density_3.sum()+density_4.sum()
         pred.append(pred_sum)
         gt_e = np.sum(groundtruth)
+        pred_e = pred_sum
         gt.append(gt_e)
-        count = 'crowded' if gt_e > 150 else 'sparse'
+        # count = 'crowded' if gt_e > 150 else 'sparse'
         error = abs(pred_sum-np.sum(groundtruth))
         if error > max_error:
             max_error = error
@@ -123,41 +125,43 @@ with torch.no_grad():
         # else:
         #     preds[2].append(pred_e)
         #     gts[2].append(gt_e)
-        if count == 'crowded':
+        # if count == 'crowded':
+        #     preds[2].append(pred_e)
+        #     gts[2].append(gt_e)
+        # else:
+        #     preds[3].append(pred_e)
+        #     gts[3].append(gt_e)
+        if angle == '60':
             preds[2].append(pred_e)
             gts[2].append(gt_e)
         else:
             preds[3].append(pred_e)
             gts[3].append(gt_e)
-        if angle == '60':
+        if bird == 'stand':
             preds[4].append(pred_e)
             gts[4].append(gt_e)
         else:
             preds[5].append(pred_e)
             gts[5].append(gt_e)
-        if bird == 'stand':
+        if size == 'small':
             preds[6].append(pred_e)
             gts[6].append(gt_e)
         else:
             preds[7].append(pred_e)
             gts[7].append(gt_e)
-        if size == 'small':
-            preds[8].append(pred_e)
-            gts[8].append(gt_e)
-        else:
-            preds[9].append(pred_e)
-            gts[9].append(gt_e)
         print('\r[{:>{}}/{}], error: {:.2f} pred: {:.2f}, gt: {:.2f}, {}'.format(i+1, len(str(len(img_paths))), len(img_paths), error, pred_sum, np.sum(groundtruth), img_paths[i]), end='')
     print('max_error: {:.2f}, min_error: {:.2f}'.format(max_error, min_error))
     
 mae = mean_absolute_error(pred,gt)
 rmse = np.sqrt(mean_squared_error(pred,gt))
+with open('test_result.txt', 'a') as f:
+    f.write('MAE: {:.2f}, RMSE: {:.2f}\n'.format(mae, rmse))
+    print ('MAE: ',mae)
+    print ('RMSE: ',rmse)
 
-print ('MAE: ',mae)
-print ('RMSE: ',rmse)
-
-attri = ['sunny', 'backlight', 'crowded', 'sparse', '60', '90', 'stand', 'fly', 'small', 'mid']
-for i in range(10):
-    if len(preds[i]) == 0:
-        continue
-    print('{}: MAE:{}. RMSE:{}.'.format(attri[i], mean_absolute_error(preds[i], gts[i]), np.sqrt(mean_squared_error(preds[i], gts[i]))))
+    attri = ['sunny', 'backlight', '60', '90', 'stand', 'fly', 'small', 'mid']
+    for i in range(8):
+        if len(preds[i]) == 0:
+            continue
+        print('{}: MAE:{}. RMSE:{}.'.format(attri[i], mean_absolute_error(preds[i], gts[i]), np.sqrt(mean_squared_error(preds[i], gts[i]))))
+        f.write('{}: MAE:{}. RMSE:{}.\n'.format(attri[i], mean_absolute_error(preds[i], gts[i]), np.sqrt(mean_squared_error(preds[i], gts[i]))))
